e63fab965facd66c22bebbc6c49c51a2a47fd90c
Ricardo Neud <ricardoneud2007@gmail.com>
2025-02-06 22:25:33 +0100

    Add files via upload

---
+++ lang/extension.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'hostname' => 'Hostnaam',
+    'hostname_description' => 'De hostnaam van het webhostingpaneel, bijvoorbeeld keyhelp.example.com. De hostnaam wordt gebruikt voor de API-verbinding.',
+    'api_version' => 'API Versie',
+    'api_version_description' => 'De API-versie die wordt gebruikt om de API te informeren welke versie je gebruikt.',
+    'api_key' => 'API Sleutel',
+    'api_key_description' => 'De API-sleutel die wordt gebruikt om verbinding te maken. Zonder de API-sleutel krijgt men een 401 Unauthorized-fout en kan de API niet worden gebruikt.',
+    'extension_language' => 'Taal van de Extensie',
+    'extension_language_description' => 'Kies de taal voor de extensie. Dit heeft invloed op de taal die in de gehele extensie wordt gebruikt.',
+    'default_password' => 'Standaard Wachtwoord',
+    'default_password_description' => 'Het standaard wachtwoord dat gebruikers voor de eerste keer gebruiken om in te loggen en het daarna te wijzigen.',
+    'hosting_plan' => 'Hosting Plan',
+    'hosting_plan_description' => 'Het hostingplan dat aan de gebruiker wordt toegewezen bij de aanmaak.',
+    'comments' => 'Opmerkingen',
+    'comments_description' => 'De opmerkingen voor het gebruikersaccount dat wordt aangemaakt. Deze optie is optioneel.',
+    'default_language' => 'Standaardtaal',
+    'default_language_description' => 'De standaardtaal die wordt ingesteld bij het aanmaken van de gebruiker',
+];
