80a0db32ee06d2196a4eab0490fc28721dc88792
Ricardo Neud <ricardoneud2007@gmail.com>
2025-02-06 22:24:51 +0100

    Add files via upload

---
+++ extension.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'hostname' => 'Hostname',
+    'hostname_description' => 'The hostname of the web host panel, for example keyhelp.example.com. The hostname is used for the API connection.',
+    'api_version' => 'API Version',
+    'api_version_description' => 'The API version used to inform the API which version you are using.',
+    'api_key' => 'API Key',
+    'api_key_description' => 'The API key used to connect. Without the API key, a 401 Unauthorized error will occur, and the API cannot be used.',
+    'extension_language' => 'Extension Language',
+    'extension_language_description' => 'Select the language for the extension. This will affect the language used throughout the extension.',
+    'default_password' => 'Default Password',
+    'default_password_description' => 'The default password users will use to log in for the first time and then change it.',
+    'hosting_plan' => 'Hosting Plan',
+    'hosting_plan_description' => 'The hosting plan assigned to the user upon creation.',
+    'comments' => 'Comments',
+    'comments_description' => 'The comments for the user account that is created. This option is optional.',
+    'default_language' => 'Default Language',
+    'default_language_description' => 'The Default Language that is set when creating the user',
+];
